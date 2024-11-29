e592dcc705f5bdbbe457e7111d6067efa0f2c987
DasunNirmal <dasun200301@gmail.com>
2024-11-29 20:09:23 +0530

    mappings for field

--- src/main/java/lk/ijse/greenshadow/utill/Mapping.java
+++ src/main/java/lk/ijse/greenshadow/utill/Mapping.java
@@ -1,4 +1,27 @@
 package lk.ijse.greenshadow.utill;
 
+import lk.ijse.greenshadow.dto.impl.FieldDtoImpl;
+import lk.ijse.greenshadow.entity.impl.FieldEntity;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    /*for field mapping*/
+    public FieldEntity toFieldEntity(FieldDtoImpl fieldDto) {
+        return modelMapper.map(fieldDto, FieldEntity.class);
+    }
+    public FieldDtoImpl toFieldDto(FieldEntity fieldEntity) {
+        return modelMapper.map(fieldEntity, FieldDtoImpl.class);
+    }
+    public List<FieldDtoImpl> toAllFields(List<FieldEntity> fieldEntityList) {
+        return modelMapper.map(fieldEntityList, new TypeToken<List<FieldDtoImpl>>() {}.getType());
+    }
 }
