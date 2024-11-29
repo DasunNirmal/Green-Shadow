4f541e16c2b9f2d3278aba224daa9aabb503851d
DasunNirmal <dasun200301@gmail.com>
2024-11-29 20:10:38 +0530

    save done

--- src/main/java/lk/ijse/greenshadow/service/impl/FieldServiceImpl.java
+++ src/main/java/lk/ijse/greenshadow/service/impl/FieldServiceImpl.java
@@ -1,4 +1,36 @@
 package lk.ijse.greenshadow.service.impl;
 
-public class FieldServiceImpl {
+import lk.ijse.greenshadow.dao.FieldDao;
+import lk.ijse.greenshadow.dto.impl.FieldDtoImpl;
+import lk.ijse.greenshadow.entity.impl.FieldEntity;
+import lk.ijse.greenshadow.exception.DataPersistException;
+import lk.ijse.greenshadow.service.FieldService;
+import lk.ijse.greenshadow.utill.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class FieldServiceImpl implements FieldService {
+    @Autowired
+    private FieldDao fieldDao;
+    @Autowired
+    private Mapping mapping;
+
+    @Override
+    public void saveFields(String fieldID, String fieldName, String fieldLocation, String fieldSize, String base67FieldImg01, String base67FieldImg02) {
+        FieldDtoImpl fieldDto = new FieldDtoImpl();
+        fieldDto.setField_code(fieldID);
+        fieldDto.setField_name(fieldName);
+        fieldDto.setField_location(fieldLocation);
+        fieldDto.setExtent_size(Double.parseDouble(fieldSize));
+        fieldDto.setImg_01(base67FieldImg01);
+        fieldDto.setImg_02(base67FieldImg02);
+
+        FieldEntity saved = fieldDao.save(mapping.toFieldEntity(fieldDto));
+        if (saved == null) {
+            throw new DataPersistException("Failed to save field");
+        }
+    }
 }
