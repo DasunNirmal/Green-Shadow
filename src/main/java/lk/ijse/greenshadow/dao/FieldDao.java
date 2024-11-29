367d5add108a2760dffdb8f83e3938c9f8f9acc8
DasunNirmal <dasun200301@gmail.com>
2024-11-29 20:17:07 +0530

    save done

--- src/main/java/lk/ijse/greenshadow/dao/FieldDao.java
+++ src/main/java/lk/ijse/greenshadow/dao/FieldDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.greenshadow.dao;
 
-public interface FieldDao {
+import lk.ijse.greenshadow.entity.impl.FieldEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FieldDao extends JpaRepository<FieldEntity,String> {
 }
