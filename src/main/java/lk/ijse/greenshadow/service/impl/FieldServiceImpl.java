0c3e72c56479a6e150ceadd67e400789f53a6c6c
DasunNirmal <dasun200301@gmail.com>
2024-11-30 22:47:19 +0530

    update done

--- src/main/java/lk/ijse/greenshadow/service/impl/FieldServiceImpl.java
+++ src/main/java/lk/ijse/greenshadow/service/impl/FieldServiceImpl.java
@@ -24,15 +24,9 @@ public class FieldServiceImpl implements FieldService {
 
     @Override
     public void saveFields(String fieldID, String fieldName, String fieldLocation, String fieldSize, String base67FieldImg01, String base67FieldImg02) {
-        FieldDtoImpl fieldDto = new FieldDtoImpl();
-        fieldDto.setField_code(fieldID);
-        fieldDto.setField_name(fieldName);
-        fieldDto.setField_location(fieldLocation);
-        fieldDto.setExtent_size(Double.parseDouble(fieldSize));
-        fieldDto.setImg_01(base67FieldImg01);
-        fieldDto.setImg_02(base67FieldImg02);
+        FieldDtoImpl dto = getFieldDto(fieldID,fieldName,fieldLocation,fieldSize,base67FieldImg01,base67FieldImg02);
 
-        FieldEntity saved = fieldDao.save(mapping.toFieldEntity(fieldDto));
+        FieldEntity saved = fieldDao.save(mapping.toFieldEntity(dto));
         if (saved == null) {
             throw new DataPersistException("Failed to save field");
         }
@@ -61,5 +55,29 @@ public class FieldServiceImpl implements FieldService {
         } else {
             fieldDao.deleteById(fieldCode);
         }
+    }
+
+    @Override
+    public void updateFields(String fieldID, String fieldName, String fieldLocation, String fieldSize, String base67FieldImg01, String base67FieldImg02) {
+        FieldDtoImpl dto = getFieldDto(fieldID,fieldName,fieldLocation,fieldSize,base67FieldImg01,base67FieldImg02);
+        Optional<FieldEntity> field = fieldDao.findById(fieldID);
+        if (field.isPresent()) {
+            field.get().setField_name(dto.getField_name());
+            field.get().setField_location(dto.getField_location());
+            field.get().setExtent_size(dto.getExtent_size());
+            field.get().setImg_01(dto.getImg_01());
+            field.get().setImg_02(dto.getImg_02());
+        }
+    }
+
+    private FieldDtoImpl getFieldDto(String fieldID, String fieldName, String fieldLocation, String fieldSize, String base67FieldImg01, String base67FieldImg02) {
+        FieldDtoImpl fieldDto = new FieldDtoImpl();
+        fieldDto.setField_code(fieldID);
+        fieldDto.setField_name(fieldName);
+        fieldDto.setField_location(fieldLocation);
+        fieldDto.setExtent_size(Double.parseDouble(fieldSize));
+        fieldDto.setImg_01(base67FieldImg01);
+        fieldDto.setImg_02(base67FieldImg02);
+        return fieldDto;
     }
 }
