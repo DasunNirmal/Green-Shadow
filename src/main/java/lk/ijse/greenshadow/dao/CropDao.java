6aa27a142c4fe05b0127561a8e706d78e7489649
DasunNirmal <dasun200301@gmail.com>
2024-12-01 20:41:11 +0530

    crop dao created

--- src/main/java/lk/ijse/greenshadow/dao/CropDao.java
+++ src/main/java/lk/ijse/greenshadow/dao/CropDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.greenshadow.dao;
 
-public interface CropDao {
+import lk.ijse.greenshadow.entity.impl.CropEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CropDao extends JpaRepository<CropEntity,String> {
 }
