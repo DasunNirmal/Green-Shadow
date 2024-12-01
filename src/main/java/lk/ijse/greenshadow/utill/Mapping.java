68be433d520db7d6f653046e7463b80e09371239
DasunNirmal <dasun200301@gmail.com>
2024-12-01 13:23:18 +0530

    mappings for staff added

--- src/main/java/lk/ijse/greenshadow/utill/Mapping.java
+++ src/main/java/lk/ijse/greenshadow/utill/Mapping.java
@@ -1,7 +1,9 @@
 package lk.ijse.greenshadow.utill;
 
 import lk.ijse.greenshadow.dto.impl.FieldDtoImpl;
+import lk.ijse.greenshadow.dto.impl.StaffDtoImpl;
 import lk.ijse.greenshadow.entity.impl.FieldEntity;
+import lk.ijse.greenshadow.entity.impl.StaffEntity;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +25,16 @@ public class Mapping {
     }
     public List<FieldDtoImpl> toAllFields(List<FieldEntity> fieldEntityList) {
         return modelMapper.map(fieldEntityList, new TypeToken<List<FieldDtoImpl>>() {}.getType());
+    }
+
+    /*for staff mapping*/
+    public StaffEntity toStaffEntity(StaffDtoImpl staffDto) {
+        return modelMapper.map(staffDto, StaffEntity.class);
+    }
+    public StaffDtoImpl toStaffDto(StaffEntity staffEntity) {
+        return modelMapper.map(staffEntity, StaffDtoImpl.class);
+    }
+    public List<StaffDtoImpl> toAllStaffs(List<StaffEntity> staffEntityList) {
+        return modelMapper.map(staffEntityList, new TypeToken<List<StaffDtoImpl>>() {}.getType());
     }
 }
