e54d175209bc74618a9f7394f6f95ad50b554cd0
DasunNirmal <dasun200301@gmail.com>
2024-12-01 23:06:04 +0530

    delete done

--- src/main/java/lk/ijse/greenshadow/service/CropService.java
+++ src/main/java/lk/ijse/greenshadow/service/CropService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface CropService {
     void saveCrops(String cropCode, String commonName, String scientificName, String category, String base67FieldImg, String season, String field_code);
     List<CropDtoImpl> loadAllCrops();
+    void deleteCrop(String cropCode);
 }
