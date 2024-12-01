f58d9d7cfb29cb637e426f52079899b6a3ede210
DasunNirmal <dasun200301@gmail.com>
2024-12-01 13:25:52 +0530

    save done

--- src/main/java/lk/ijse/greenshadow/controller/StaffController.java
+++ src/main/java/lk/ijse/greenshadow/controller/StaffController.java
@@ -1,4 +1,31 @@
 package lk.ijse.greenshadow.controller;
 
+import lk.ijse.greenshadow.dto.impl.StaffDtoImpl;
+import lk.ijse.greenshadow.exception.DataPersistException;
+import lk.ijse.greenshadow.service.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:63342")
+@RestController
+@RequestMapping("api/v1/staff")
 public class StaffController {
+    @Autowired
+    private StaffService staffService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveStaffs(@RequestBody StaffDtoImpl staffDto) {
+        try {
+            System.out.println(staffDto);
+            staffService.saveStaff(staffDto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
