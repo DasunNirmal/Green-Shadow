6c8383ac5ada4fa9e749c548f8e98d18633e0916
DasunNirmal <dasun200301@gmail.com>
2024-12-01 20:58:12 +0530

    get all done

--- src/main/java/lk/ijse/greenshadow/service/CropService.java
+++ src/main/java/lk/ijse/greenshadow/service/CropService.java
@@ -1,5 +1,10 @@
 package lk.ijse.greenshadow.service;
 
+import lk.ijse.greenshadow.dto.impl.CropDtoImpl;
+
+import java.util.List;
+
 public interface CropService {
     void saveCrops(String cropCode, String commonName, String scientificName, String category, String base67FieldImg, String season, String field_code);
+    List<CropDtoImpl> loadAllCrops();
 }
