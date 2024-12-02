71191ff0bcc12d7e30d9bca83f791339dc8679f6
DasunNirmal <dasun200301@gmail.com>
2024-12-02 11:42:49 +0530

    save and get done

--- src/main/java/lk/ijse/greenshadow/service/impl/StaffAndFieldDetailsServiceImpl.java
+++ src/main/java/lk/ijse/greenshadow/service/impl/StaffAndFieldDetailsServiceImpl.java
@@ -1,4 +1,35 @@
 package lk.ijse.greenshadow.service.impl;
 
-public interface StaffAndFieldDetailsServiceImpl {
+import lk.ijse.greenshadow.dao.StaffFieldDetailsDao;
+import lk.ijse.greenshadow.dto.impl.StaffFiledDtoImpl;
+import lk.ijse.greenshadow.entity.impl.FieldStaffDetailsEntity;
+import lk.ijse.greenshadow.exception.DataPersistException;
+import lk.ijse.greenshadow.service.StaffAndFieldDetailsService;
+import lk.ijse.greenshadow.utill.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class StaffAndFieldDetailsServiceImpl implements StaffAndFieldDetailsService {
+    @Autowired
+    private StaffFieldDetailsDao staffFieldDetailsDao;
+    @Autowired
+    private Mapping mapping;
+
+    @Override
+    public void saveDetails(StaffFiledDtoImpl staffFiledDto) {
+        FieldStaffDetailsEntity save = staffFieldDetailsDao.save(mapping.toStaffFiledDetailsEntity(staffFiledDto));
+        if (save == null) {
+            throw new DataPersistException("Save Staff Failed");
+        }
+    }
+
+    @Override
+    public List<StaffFiledDtoImpl> loadAllStaffDetails() {
+        return mapping.toAllStaffFiledDetails(staffFieldDetailsDao.findAll());
+    }
 }
