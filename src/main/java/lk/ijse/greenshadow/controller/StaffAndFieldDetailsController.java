013a4f9c66e0cd6c4c7c79b94f60e22396765914
DasunNirmal <dasun200301@gmail.com>
2024-12-02 11:46:36 +0530

    save and get done

--- src/main/java/lk/ijse/greenshadow/controller/StaffAndFieldDetailsController.java
+++ src/main/java/lk/ijse/greenshadow/controller/StaffAndFieldDetailsController.java
@@ -1,4 +1,38 @@
 package lk.ijse.greenshadow.controller;
 
-public class StaffAndFieldDetialsController {
+import lk.ijse.greenshadow.dto.impl.StaffFiledDtoImpl;
+import lk.ijse.greenshadow.exception.DataPersistException;
+import lk.ijse.greenshadow.service.StaffAndFieldDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:63342")
+@RestController
+@RequestMapping("api/v1/staffAndFieldsDetails")
+public class StaffAndFieldDetailsController {
+    @Autowired
+    private StaffAndFieldDetailsService staffAndFieldDetailsService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveStaffs(@RequestBody StaffFiledDtoImpl staffFiledDto) {
+        try {
+            System.out.println(staffFiledDto);
+            staffAndFieldDetailsService.saveDetails(staffFiledDto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<StaffFiledDtoImpl> getAllStaff() {
+        return staffAndFieldDetailsService.loadAllStaffDetails();
+    }
 }
