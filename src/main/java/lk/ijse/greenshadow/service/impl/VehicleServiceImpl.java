f374c6d9a731d0094fc06cde940029539ed525da
DasunNirmal <dasun200301@gmail.com>
2024-12-02 21:01:24 +0530

    update done

--- src/main/java/lk/ijse/greenshadow/service/impl/VehicleServiceImpl.java
+++ src/main/java/lk/ijse/greenshadow/service/impl/VehicleServiceImpl.java
@@ -1,9 +1,12 @@
 package lk.ijse.greenshadow.service.impl;
 
+import lk.ijse.greenshadow.dao.StaffDao;
 import lk.ijse.greenshadow.dao.VehicleDao;
 import lk.ijse.greenshadow.dto.impl.VehicleDtoImpl;
+import lk.ijse.greenshadow.entity.impl.StaffEntity;
 import lk.ijse.greenshadow.entity.impl.VehicleEntity;
 import lk.ijse.greenshadow.exception.DataPersistException;
+import lk.ijse.greenshadow.exception.StaffNotFoundException;
 import lk.ijse.greenshadow.exception.VehicleNotFoundException;
 import lk.ijse.greenshadow.service.VehicleService;
 import lk.ijse.greenshadow.utill.Mapping;
@@ -19,6 +22,8 @@ import java.util.Optional;
 public class VehicleServiceImpl implements VehicleService {
     @Autowired
     private VehicleDao vehicleDao;
+    @Autowired
+    private StaffDao staffDao;
     @Autowired
     private Mapping mapping;
 
@@ -42,6 +47,30 @@ public class VehicleServiceImpl implements VehicleService {
             throw new VehicleNotFoundException("Vehicle Not Found");
         } else {
             vehicleDao.deleteById(vehicleCode);
+        }
+    }
+
+    @Override
+    public void updateVehicles(String vehicleCode, VehicleDtoImpl vehicleDto) {
+        Optional<VehicleEntity> vehicle = vehicleDao.findById(vehicleCode);
+        if (vehicle.isPresent()) {
+            vehicle.get().setLicense_plate(vehicleDto.getLicense_plate());
+            vehicle.get().setFuel_type(vehicleDto.getFuel_type());
+            vehicle.get().setRole(vehicleDto.getRole());
+            vehicle.get().setRemarks(vehicleDto.getRemarks());
+            vehicle.get().setVehicle_category(vehicleDto.getVehicle_category());
+            vehicle.get().setStatus(vehicleDto.getStatus());
+            vehicle.get().setFirst_name(vehicleDto.getFirst_name());
+            vehicle.get().setPhone_no(vehicleDto.getPhone_no());
+            vehicle.get().setEmail(vehicleDto.getEmail());
+
+            if (vehicleDto.getStaff_id() != null) {
+                Optional<StaffEntity> staff = staffDao.findById(vehicleDto.getStaff_id());
+                vehicle.get().setStaff(staff.orElse(null)); /*set staff if provided or null*/
+            } else {
+                vehicle.get().setStaff(null);
+            }
+            vehicle.get().setVehicle_code(vehicleCode);
         }
     }
 }
