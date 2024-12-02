427c4532b851e74e070c5675604def78da353a47
DasunNirmal <dasun200301@gmail.com>
2024-12-02 11:46:15 +0530

    updated

--- src/main/java/lk/ijse/greenshadow/dao/StaffDao.java
+++ src/main/java/lk/ijse/greenshadow/dao/StaffDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StaffDao  extends JpaRepository<StaffEntity,String> {
+public interface StaffDao extends JpaRepository<StaffEntity,String> {
 }
