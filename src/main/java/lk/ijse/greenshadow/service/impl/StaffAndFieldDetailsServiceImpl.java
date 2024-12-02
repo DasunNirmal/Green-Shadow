403192e4635f4712c656dde5109a254b77981b30
DasunNirmal <dasun200301@gmail.com>
2024-12-02 11:59:14 +0530

    Delete Done

--- src/main/java/lk/ijse/greenshadow/service/impl/StaffAndFieldDetailsServiceImpl.java
+++ src/main/java/lk/ijse/greenshadow/service/impl/StaffAndFieldDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import lk.ijse.greenshadow.dao.StaffFieldDetailsDao;
 import lk.ijse.greenshadow.dto.impl.StaffFiledDtoImpl;
 import lk.ijse.greenshadow.entity.impl.FieldStaffDetailsEntity;
 import lk.ijse.greenshadow.exception.DataPersistException;
+import lk.ijse.greenshadow.exception.DetailsNotFoundException;
 import lk.ijse.greenshadow.service.StaffAndFieldDetailsService;
 import lk.ijse.greenshadow.utill.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -31,5 +33,14 @@ public class StaffAndFieldDetailsServiceImpl implements StaffAndFieldDetailsServ
     @Override
     public List<StaffFiledDtoImpl> loadAllStaffDetails() {
         return mapping.toAllStaffFiledDetails(staffFieldDetailsDao.findAll());
+    }
+
+    @Override
+    public void deleteDetails(String detailsId) {
+        Optional<FieldStaffDetailsEntity> detailsFound = staffFieldDetailsDao.findById(detailsId);
+        if (detailsFound.isEmpty()) {
+            throw new DetailsNotFoundException("Details not found");
+        }
+        staffFieldDetailsDao.deleteById(detailsId);
     }
 }
