8a8245cf45262f9a93f3c6b9a8d8dc126b5a8d94
DasunNirmal <dasun200301@gmail.com>
2024-12-02 11:59:28 +0530

    Delete Done

--- src/main/java/lk/ijse/greenshadow/service/StaffAndFieldDetailsService.java
+++ src/main/java/lk/ijse/greenshadow/service/StaffAndFieldDetailsService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface StaffAndFieldDetailsService {
     void saveDetails(StaffFiledDtoImpl staffFiledDto);
     List<StaffFiledDtoImpl> loadAllStaffDetails();
+    void deleteDetails(String detailsId);
 }
