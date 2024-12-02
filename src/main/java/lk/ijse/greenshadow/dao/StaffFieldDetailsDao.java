b224c72278741564b049c20c304f361d39d87163
DasunNirmal <dasun200301@gmail.com>
2024-12-02 11:45:54 +0530

    updated

--- src/main/java/lk/ijse/greenshadow/dao/StaffFieldDetailsDao.java
+++ src/main/java/lk/ijse/greenshadow/dao/StaffFieldDetailsDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.greenshadow.dao;
 
-public interface StaffFieldDetailsDao {
+import lk.ijse.greenshadow.entity.impl.FieldStaffDetailsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffFieldDetailsDao extends JpaRepository<FieldStaffDetailsEntity,String> {
 }
