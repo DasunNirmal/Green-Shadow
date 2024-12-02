b143163bda634d1e61bb1fd2ea364d0e116d2c44
DasunNirmal <dasun200301@gmail.com>
2024-12-02 20:07:43 +0530

    save,get and delete done

--- src/main/java/lk/ijse/greenshadow/controller/VehicleController.java
+++ src/main/java/lk/ijse/greenshadow/controller/VehicleController.java
@@ -1,4 +1,50 @@
 package lk.ijse.greenshadow.controller;
 
+import lk.ijse.greenshadow.dto.impl.VehicleDtoImpl;
+import lk.ijse.greenshadow.exception.CropNotFoundException;
+import lk.ijse.greenshadow.exception.DataPersistException;
+import lk.ijse.greenshadow.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:63342")
+@RestController
+@RequestMapping("api/v1/vehicle")
 public class VehicleController {
+    @Autowired
+    private VehicleService vehicleService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveVehicles(@RequestBody VehicleDtoImpl vehicleDto) {
+        try {
+            vehicleService.saveVehicle(vehicleDto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<VehicleDtoImpl> getAllVehicles() {
+        return vehicleService.loadAllVehicles();
+    }
+
+    @DeleteMapping(value = "/{vehicle_code}")
+    public ResponseEntity<Void> deleteVehicle(@PathVariable("vehicle_code") String vehicleCode) {
+        try {
+            vehicleService.deleteVehicle(vehicleCode);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (CropNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
