6724e7554d013ab34379f207a24b900b8a6266f2
DasunNirmal <dasun200301@gmail.com>
2024-12-02 20:07:11 +0530

    done

--- src/main/java/lk/ijse/greenshadow/dao/VehicleDao.java
+++ src/main/java/lk/ijse/greenshadow/dao/VehicleDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.greenshadow.dao;
 
-public interface VehicleDao {
+import lk.ijse.greenshadow.entity.impl.VehicleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleDao extends JpaRepository<VehicleEntity,String> {
 }
