2f8595157d63784691100e873027be00e534a840
DasunNirmal <dasun200301@gmail.com>
2024-12-04 20:22:16 +0530

    get all done

--- src/main/java/lk/ijse/greenshadow/service/EquipmentService.java
+++ src/main/java/lk/ijse/greenshadow/service/EquipmentService.java
@@ -2,6 +2,9 @@ package lk.ijse.greenshadow.service;
 
 import lk.ijse.greenshadow.dto.impl.EquipmentDtoImpl;
 
+import java.util.List;
+
 public interface EquipmentService {
     void saveEquipment(EquipmentDtoImpl equipmentDto);
+    List<EquipmentDtoImpl> loadAllEquipments();
 }
