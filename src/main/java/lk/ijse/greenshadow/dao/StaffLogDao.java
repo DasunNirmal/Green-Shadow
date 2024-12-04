90f87b26d6f9968f3e217e0d0931ca59d6c47dd8
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:22:35 +0530

    interfaces

--- src/main/java/lk/ijse/greenshadow/dao/StaffLogDao.java
+++ src/main/java/lk/ijse/greenshadow/dao/StaffLogDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.greenshadow.dao;
 
-public interface StaffLogDao {
+import lk.ijse.greenshadow.entity.impl.StaffMonitoringDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffLogDao extends JpaRepository<StaffMonitoringDetails, String> {
 }
