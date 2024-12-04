fe6c3594c8dfd0b565d8156add926e4fe82f3de4
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:17:32 +0530

    file created

--- src/main/java/lk/ijse/greenshadow/service/LogService.java
+++ src/main/java/lk/ijse/greenshadow/service/LogService.java
@@ -1,4 +1,5 @@
 package lk.ijse.greenshadow.service;
 
 public interface LogService {
+    void saveLogs(String logCode, String base67Img, String details, String logDate, String fieldCode, String fieldName, String fieldLocation);
 }
