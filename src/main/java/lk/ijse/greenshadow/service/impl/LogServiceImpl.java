5eecba1d60bec2ed458425cf00a4bb8d0385873d
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:16:24 +0530

    save done

--- src/main/java/lk/ijse/greenshadow/service/impl/LogServiceImpl.java
+++ src/main/java/lk/ijse/greenshadow/service/impl/LogServiceImpl.java
@@ -1,4 +1,38 @@
 package lk.ijse.greenshadow.service.impl;
 
-public class LogServiceImpl {
+import lk.ijse.greenshadow.dao.LogDao;
+import lk.ijse.greenshadow.dto.impl.LogDtoImpl;
+import lk.ijse.greenshadow.entity.impl.MonitoringLogEntity;
+import lk.ijse.greenshadow.exception.DataPersistException;
+import lk.ijse.greenshadow.service.LogService;
+import lk.ijse.greenshadow.utill.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class LogServiceImpl implements LogService {
+    @Autowired
+    private LogDao logDao;
+    @Autowired
+    private Mapping mapping;
+
+    @Override
+    public void saveLogs(String logCode, String base67Img, String details, String logDate, String fieldCode, String fieldName, String fieldLocation) {
+        LogDtoImpl logDto = getLogDto(logCode,base67Img,details,logDate,fieldCode,fieldName,fieldLocation);
+        MonitoringLogEntity save = logDao.save(mapping.toLogEntity(logDto));
+        if (save == null) {
+            throw new DataPersistException("Failed to save log");
+        }
+    }
+
+    private LogDtoImpl getLogDto(String logCode, String base67Img, String details, String logDate, String fieldCode, String fieldName, String fieldLocation) {
+        LogDtoImpl logDto = new LogDtoImpl();
+        logDto.setLog_code(logCode);
+        logDto.setImg(base67Img);
+        logDto.setDetails(details);
+        logDto.setLog_date(logDate);
+        return logDto;
+    }
 }
