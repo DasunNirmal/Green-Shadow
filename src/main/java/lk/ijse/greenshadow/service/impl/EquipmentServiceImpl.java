817d9010bca7f919bc8c238347f2a17f04ccabe1
DasunNirmal <dasun200301@gmail.com>
2024-12-04 20:21:59 +0530

    get all done

--- src/main/java/lk/ijse/greenshadow/service/impl/EquipmentServiceImpl.java
+++ src/main/java/lk/ijse/greenshadow/service/impl/EquipmentServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -56,5 +57,10 @@ public class EquipmentServiceImpl implements EquipmentService {
         }
 
         equipmentDao.save(equipmentEntity);
+    }
+
+    @Override
+    public List<EquipmentDtoImpl> loadAllEquipments() {
+        return mapping.toAllEquipments(equipmentDao.findAll());
     }
 }
