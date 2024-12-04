7e78bb89e88d923464140bac4f6261c35692b930
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:17:11 +0530

    file created

--- src/main/java/lk/ijse/greenshadow/service/impl/CropLogServiceImpl.java
+++ src/main/java/lk/ijse/greenshadow/service/impl/CropLogServiceImpl.java
@@ -1,4 +1,10 @@
 package lk.ijse.greenshadow.service.impl;
 
-public class CropLogServiceImpl {
+import lk.ijse.greenshadow.service.CropLogService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CropLogServiceImpl implements CropLogService {
 }
