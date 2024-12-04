461a13b36fe188b9fefd896b6a9fced6d7be8164
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:22:47 +0530

    interfaces

--- src/main/java/lk/ijse/greenshadow/dao/LogDao.java
+++ src/main/java/lk/ijse/greenshadow/dao/LogDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.greenshadow.dao;
 
-public interface LogDao {
+import lk.ijse.greenshadow.entity.impl.MonitoringLogEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogDao extends JpaRepository<MonitoringLogEntity, String> {
 }
