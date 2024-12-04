2575b67cab040302aa2cda9a3c01ffd9c4785752
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:15:50 +0530

    save done

--- src/main/java/lk/ijse/greenshadow/service/impl/StaffLogServiceImpl.java
+++ src/main/java/lk/ijse/greenshadow/service/impl/StaffLogServiceImpl.java
@@ -1,4 +1,10 @@
 package lk.ijse.greenshadow.service.impl;
 
-public class StaffLogServiceImpl {
+import lk.ijse.greenshadow.service.StaffLogService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class StaffLogServiceImpl implements StaffLogService {
 }
