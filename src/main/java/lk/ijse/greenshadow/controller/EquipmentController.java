f644d15e3dc1a1df0474d4792e7c9cdc5e867365
DasunNirmal <dasun200301@gmail.com>
2024-12-04 08:38:38 +0530

    save done

--- src/main/java/lk/ijse/greenshadow/controller/EquipmentController.java
+++ src/main/java/lk/ijse/greenshadow/controller/EquipmentController.java
@@ -1,4 +1,30 @@
 package lk.ijse.greenshadow.controller;
 
+import lk.ijse.greenshadow.dto.impl.EquipmentDtoImpl;
+import lk.ijse.greenshadow.exception.DataPersistException;
+import lk.ijse.greenshadow.service.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:63342")
+@RestController
+@RequestMapping("api/v1/equipment")
 public class EquipmentController {
+    @Autowired
+    private EquipmentService equipmentService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveEquipment(@RequestBody EquipmentDtoImpl equipmentDto) {
+        try {
+            equipmentService.saveEquipment(equipmentDto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
