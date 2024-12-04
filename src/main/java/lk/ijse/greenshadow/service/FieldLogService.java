40a57e3914fc98ff5c2678f486ea04a3207cb51e
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:49:53 +0530

    get all done

--- src/main/java/lk/ijse/greenshadow/service/FieldLogService.java
+++ src/main/java/lk/ijse/greenshadow/service/FieldLogService.java
@@ -1,5 +1,10 @@
 package lk.ijse.greenshadow.service;
 
+import lk.ijse.greenshadow.dto.impl.FieldLogDtoImpl;
+
+import java.util.List;
+
 public interface FieldLogService {
     void saveDetails(String logCode, String base67Img, String details, String logDate, String fieldCode, String fieldName, String fieldLocation);
+    List<FieldLogDtoImpl> loadAllDetails();
 }
