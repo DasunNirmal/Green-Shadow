db3df7030a4c3b30fbd5ee4a25d2530843a970a9
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:17:47 +0530

    file created

--- src/main/java/lk/ijse/greenshadow/service/FieldLogService.java
+++ src/main/java/lk/ijse/greenshadow/service/FieldLogService.java
@@ -1,4 +1,5 @@
 package lk.ijse.greenshadow.service;
 
 public interface FieldLogService {
+    void saveDetails(String logCode, String base67Img, String details, String logDate, String fieldCode, String fieldName, String fieldLocation);
 }
