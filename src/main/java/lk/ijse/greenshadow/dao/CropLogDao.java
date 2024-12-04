bf2754ede6cc60760d6855e2a6a9eb576206686f
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:23:18 +0530

    interfaces

--- src/main/java/lk/ijse/greenshadow/dao/CropLogDao.java
+++ src/main/java/lk/ijse/greenshadow/dao/CropLogDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.greenshadow.dao;
 
-public interface CropLogDao {
+import lk.ijse.greenshadow.entity.impl.CropMonitoringDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CropLogDao  extends JpaRepository<CropMonitoringDetails, String> {
 }
