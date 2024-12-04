6bb61aec979f4d02015c54247638da0f16bea829
DasunNirmal <dasun200301@gmail.com>
2024-12-04 08:38:10 +0530

    done

--- src/main/java/lk/ijse/greenshadow/dao/EquipmentDao.java
+++ src/main/java/lk/ijse/greenshadow/dao/EquipmentDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.greenshadow.dao;
 
-public interface EquipmentDao {
+import lk.ijse.greenshadow.entity.impl.EquipmentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipmentDao extends JpaRepository<EquipmentEntity, String> {
 }
