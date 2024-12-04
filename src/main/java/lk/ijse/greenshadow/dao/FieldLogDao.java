4c689cadf3442930b8112a5d328d603d913188f7
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:23:05 +0530

    interfaces

--- src/main/java/lk/ijse/greenshadow/dao/FieldLogDao.java
+++ src/main/java/lk/ijse/greenshadow/dao/FieldLogDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.greenshadow.dao;
 
-public interface FieldLogDao {
+import lk.ijse.greenshadow.entity.impl.FieldMonitoringDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FieldLogDao  extends JpaRepository<FieldMonitoringDetails, String> {
 }
