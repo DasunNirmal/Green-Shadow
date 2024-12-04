90df30d16ff88957301dea7817e78c54d9c3fb1a
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:24:00 +0530

    save done

--- src/main/java/lk/ijse/greenshadow/controller/LogsController.java
+++ src/main/java/lk/ijse/greenshadow/controller/LogsController.java
@@ -1,4 +1,38 @@
 package lk.ijse.greenshadow.controller;
 
+import lk.ijse.greenshadow.service.LogService;
+import lk.ijse.greenshadow.utill.AppUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@CrossOrigin(origins = "http://localhost:63342")
+@RestController
+@RequestMapping("api/v1/logs")
 public class LogsController {
+    @Autowired
+    private LogService logService;
+
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveLogs(@RequestPart("log_code") String log_code, @RequestPart("img") MultipartFile img, @RequestPart("details") String details, @RequestPart("log_date") String log_date,
+                                          @RequestPart("field_code") String field_code,
+                                          @RequestPart("field_name") String field_name, @RequestPart("field_location") String field_location) {
+        String base67Img = "";
+
+        try {
+            byte[] imageBytes = img.getBytes();
+            base67Img = AppUtil.imageToBase64(imageBytes);
+
+            logService.saveLogs(log_code,base67Img,details,log_date,field_code,field_name,field_location);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
