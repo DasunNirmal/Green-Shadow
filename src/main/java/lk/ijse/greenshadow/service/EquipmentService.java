ed2f87c449df7a6779c85cdeb1db166f02c5f1dd
DasunNirmal <dasun200301@gmail.com>
2024-12-04 20:36:54 +0530

    delete done

--- src/main/java/lk/ijse/greenshadow/service/EquipmentService.java
+++ src/main/java/lk/ijse/greenshadow/service/EquipmentService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface EquipmentService {
     void saveEquipment(EquipmentDtoImpl equipmentDto);
     List<EquipmentDtoImpl> loadAllEquipments();
+    void deleteEquipment(String equipmentCode);
 }
