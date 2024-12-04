a665e0ad987b97e1ef8adb355cd78176664c5310
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:24:38 +0530

    controller

--- src/main/java/lk/ijse/greenshadow/controller/CropLogsController.java
+++ src/main/java/lk/ijse/greenshadow/controller/CropLogsController.java
@@ -1,4 +1,11 @@
 package lk.ijse.greenshadow.controller;
 
-public class CropLogsControllre {
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:63342")
+@RestController
+@RequestMapping("api/v1/cropLogs")
+public class CropLogsController {
 }
