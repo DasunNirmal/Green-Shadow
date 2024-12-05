14d48da59e2c6c4d0171107202c5a2225af16c23
DasunNirmal <dasun200301@gmail.com>
2024-12-05 10:43:33 +0530

    delete done

--- src/main/java/lk/ijse/greenshadow/service/FieldLogService.java
+++ src/main/java/lk/ijse/greenshadow/service/FieldLogService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface FieldLogService {
     void saveDetails(String logCode, String base67Img, String details, String logDate, String fieldCode, String fieldName, String fieldLocation);
     List<FieldLogDtoImpl> loadAllDetails();
+    void deleteDetails(String detailsID);
 }
