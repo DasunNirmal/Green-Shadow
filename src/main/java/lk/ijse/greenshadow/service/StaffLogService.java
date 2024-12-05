ee5f97dfaa9cb2bc1e69e14411cd24da26faae24
DasunNirmal <dasun200301@gmail.com>
2024-12-05 21:02:41 +0530

    get all done

--- src/main/java/lk/ijse/greenshadow/service/StaffLogService.java
+++ src/main/java/lk/ijse/greenshadow/service/StaffLogService.java
@@ -1,5 +1,11 @@
 package lk.ijse.greenshadow.service;
 
+import lk.ijse.greenshadow.dto.impl.FieldLogDtoImpl;
+import lk.ijse.greenshadow.dto.impl.StaffLogDtoImpl;
+
+import java.util.List;
+
 public interface StaffLogService {
     void saveDetails(String logCode, String base67Img, String details, String logDate, String staffId, String firstName, String phoneNo);
+    List<StaffLogDtoImpl> loadAllDetails();
 }
