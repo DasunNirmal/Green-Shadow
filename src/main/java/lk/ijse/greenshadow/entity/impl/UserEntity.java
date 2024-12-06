33d21616bcbd1848dec9033a2e03371083986d1e
DasunNirmal <dasun200301@gmail.com>
2024-12-06 10:34:12 +0530

    done

--- src/main/java/lk/ijse/greenshadow/entity/impl/UserEntity.java
+++ src/main/java/lk/ijse/greenshadow/entity/impl/UserEntity.java
@@ -6,16 +6,35 @@ import lk.ijse.greenshadow.entity.SuperEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
 @Table(name = "user")
-public class UserEntity implements SuperEntity {
+public class UserEntity implements UserDetails, SuperEntity {
     @Id
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role.name()));
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
